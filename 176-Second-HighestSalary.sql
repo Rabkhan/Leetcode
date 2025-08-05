015994a5474b23d7bb9881dc2f1cbdb650a14e89
“Rab <rabkhan25@gmail.comgit config --global user.name Rab Khangit config --global user.email rabkhan25@gmail.com>
2025-08-05 17:41:06 +0530

    Firt-commit

---
+++ 176-Second-HighestSalary.sql
@@ -0,0 +1,12 @@
+SELECT 
+CASE 
+WHEN COUNT (DISTINCT salary )> 1 THEN 
+(
+SELECT DISTINCT salary 
+FROM Employee
+ORDER BY salary DESC
+LIMIT 1  OFFSET 1
+) ELSE NULL
+
+END AS SecondHighestSalary
+FROM Employee;
