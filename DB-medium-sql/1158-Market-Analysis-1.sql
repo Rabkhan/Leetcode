260dc0f75d09be0d4c081318396c2fc1c81f4016
“Rab <rabkhan25@gmail.com>
2025-08-12 23:06:25 +0530

    1158 update

--- DB-medium-sql/1158-Market-Analysis-1.sql
+++ DB-medium-sql/1158-Market-Analysis-1.sql
@@ -1,3 +1,19 @@
+SELECT 
+  u.user_id AS buyer_id, 
+  join_date, 
+  COUNT(o.order_id) AS orders_in_2019 
+FROM 
+  Users u 
+  LEFT JOIN Orders o ON u.user_id = o.buyer_id 
+  AND EXTRACT(YEAR FROM order_date::Date)= 2019
+
+GROUP BY 
+  u.user_id, join_date
+ORDER BY 
+  u.user_id
+
+--- Above worked grouping join_date was annoying. 
+
 
 /*
 After solving this question i feel so stupid; I AM STUPID (IN CHARLES L.VOICE).
