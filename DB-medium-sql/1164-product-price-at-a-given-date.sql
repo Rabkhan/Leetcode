808b0bc0e1cd07def2cfa6899935a3d9998e0a38
“Rab <rabkhan25@gmail.com>
2025-08-13 21:06:53 +0530

    1164UP

--- DB-medium-sql/1164-product-price-at-a-given-date.sql
+++ DB-medium-sql/1164-product-price-at-a-given-date.sql
@@ -1,4 +1,26 @@
-
+WITH product_latest_price AS (
+  SELECT
+    product_id,
+    new_price,
+    change_date,
+    RANK() OVER (
+      PARTITION BY product_id
+      ORDER BY change_date DESC
+    ) AS rnk
+  FROM Products
+  WHERE change_date <= '2019-08-16'
+)
+SELECT
+  p.product_id,
+  COALESCE(plp.new_price, 10) AS price
+FROM
+  (SELECT DISTINCT product_id FROM Products) p
+  LEFT JOIN (
+    SELECT product_id, new_price
+    FROM product_latest_price
+    WHERE rnk = 1
+  ) plp
+  ON p.product_id = plp.product_id;
 
 
 /*
